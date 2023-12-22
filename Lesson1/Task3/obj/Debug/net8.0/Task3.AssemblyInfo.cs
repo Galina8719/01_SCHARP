607c42bbacbadfc85941a69f5f128aeafcc3da41
galna <galina_logistika@mail.ru>
2023-12-22 16:18:10 +0300

    промежуточное сохранение

--- Lesson1/Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
+++ Lesson1/Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05fe452f16b3945c99c204aca92b09cc2877e3ad")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cd29d03003cf5d361811b7deab4ff6c421b1aab")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
