0d9531de543ae263209fdf19b3c542bfbff822f2
galna <galina_logistika@mail.ru>
2023-12-22 15:43:26 +0300

    произведение разных типов

--- Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4bc10e50c10b4712da8db0d352cc50258a771a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b5e4849cf9fce3710bff96043033c9fce0d82b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
