163dc7733e45188b855a44320ef0056840477aa3
galna <galina_logistika@mail.ru>
2024-01-19 20:03:00 +0300

    commit

--- Lesson3/Task6/Program.cs
+++ Lesson3/Task6/Program.cs
@@ -1,26 +1,72 @@
-﻿int n = 10;                     //Создание массива
-int [] arr = new int [n];       //Создание массива
-int i = 0;                      //Создание массива
-while (i<n)
+﻿// int n = 10;                      //Создание массива
+// int[] arr = new int[n];
+// int i = 0;
+
+// while (i < n)                    //заполнение массива
+// {
+//     arr[i] = i + 1;
+// }
+// i = 0;
+
+// while (i < n)                    //вывод массива
+// {
+//     Console.WriteLine($"{arr[i]} ");
+// }
+// i = 0;
+// sum = 0;
+
+// while (i < n)                    //сумма элементов
+// {
+//     sum = sum + arr[i];
+// }
+// i = 0;
+// product = 1;
+
+// while (i < n)                    //произведение
+// {
+//     product = product * arr[i];
+// }
+
+// Console.WriteLine(sum);
+// Console.WriteLine(product);
+
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+void FillArray()                    //заполняющий массив
 {
-    arr [i] = i + 1;
+    while (i < n)
+    {
+        arr[i] = i + 1;
+        i++;
+    }
 }
-i = 0;
-while (i<n)
+
+void PrintArray()                   //вывод массива на экран
 {
-    Console.WriteLine($"{arr[i]} ");
+    while (i < n)
+    {
+        Console.WriteLine($"{arr[i]} ");
+        i++;
+    }
 }
-i = 0;
-sum = 0;
-while (i<n)
+int sum = GetSumOfElements()        //сумма
 {
-    sum = sum +arr[i];
+    while (i < n)
+    {
+        sum = sum + arr[i];
+        i++;
+    }
 }
-i = 0;
-product = 1;
-while (i<n)
+
+int product = GetProductOfElements();
 {
-    product = product *arr[i];
+    int product = 1;
+    while (i < n)
+    {
+        product = product * arr[i];
+    }
 }
 Console.WriteLine(sum);
 Console.WriteLine(product);
