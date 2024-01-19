15e9fd0dc7c33685696a163d7484a7c6002dc7dd
galna <galina_logistika@mail.ru>
2024-01-19 16:29:36 +0300

    commit

--- Lesson3/Task2/Program.cs
+++ Lesson3/Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Домашнее задание
+//Вычислить значение формулы (a*b)/(c+d), где a,b,c,d - некоторые целые числа.
+//Результат выыести на экран.
+
+double CalculateFormyla(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c * d;
+    double result = numenator / denomenator;
+    return result;
+}
+double result = CalculateFormyla(1, 2, 3, 4);
+Console.WriteLine(result);
