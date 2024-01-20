aad7abea284383b784a54e8922570593d375bd24
galna <galina_logistika@mail.ru>
2024-01-21 00:50:05 +0300

    commit

--- Lesson4/Task1/Program.cs
+++ Lesson4/Task1/Program.cs
@@ -1,2 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int n = 10;                      //Создание массива
+// int[] arr = new int[n];
+// int i = 0;
+
+// while (i < n)                    //заполнение массива
+// {
+//     arr[i] = i + 1;
+// }
+// i = 0;
+
+// while (i < n)                    //вывод массива
+// {
+//     Console.WriteLine($"{arr[i]} ");
+// }
+// i = 0;
+// sum = 0;
+
+// while (i < n)                    //сумма элементов
+// {
+//     sum = sum + arr[i];
+// }
+// i = 0;
+// product = 1;
+
+// while (i < n)                    //произведение
+// {
+//     product = product * arr[i];
+// }
+
+// Console.WriteLine(sum);
+// Console.WriteLine(product);
+
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+int FillArray(int[] arr)
+{
+    while (i < n)
+    {
+        arr[i] = i + 1;
+        Console.Write($"{arr[i]} ");
+        i = i + 1;
+    }
+    return arr[i];
+}
+FillArray(arr);
+// while (i < n)
+//     {
+//         arr[i] = i + 1;
+//         Console.Write($"{arr[i]} ");
+//         i = i + 1;
+//     }
+
+
+// void FillArray()                    //заполняющий массив
+// {
+//     while (i < n)
+//     {
+//         arr[i] = i + 1;
+//         i = i + 1;
+//     }
+// }
+
+// void PrintArray()                   //вывод массива на экран
+// {
+//     while (i < n)
+//     {
+//         Console.WriteLine($"{arr[i]} ");
+//         i = i + 1;
+//     }
+// }
+
+// int sum = GetSumOfElements()        //сумма
+// {
+//     while (i < n)
+//     {
+//         sum = sum + arr[i];
+//         i++;
+//     }
+// }
+
+// int product = GetProductOfElements()
+// {
+//     int product = 1;
+//     while (i < n)
+//     {
+//         product = product * arr[i];
+//     }
+// }
+// FillArray(arr);
+// //PrintArray(arr);
+// // GetSumOfElements(arr);
+// // GetProductOfElements(arr);
+// //Console.WriteLine(sum);
+// //Console.WriteLine(product);
