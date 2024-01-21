9f750ffa4eb64eef129e900cb44c76bfb4aca91a
galna <galina_logistika@mail.ru>
2024-01-21 16:53:14 +0300

    commit

--- Lesson3/Task6/Program.cs
+++ Lesson3/Task6/Program.cs
@@ -1,42 +1,41 @@
-﻿// int n = 10;                      //Создание массива
-// int[] arr = new int[n];
-
-// int i = 0;
-
-// while (i < n)                    //заполнение массива
-// {
-//     arr[i] = i + 1;
-// }
-
-// i = 0;
-
+﻿int n = 10;                      //Создание массива
+int[] arr = new int[n];
+int i = 0;
+int sum = 0;
+//int product = 1;
+while (i < n)                    //заполнение массива
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} ");
+    i++;
+}
+Console.WriteLine();
+//int i = 0;
 // while (i < n)                    //вывод массива
 // {
 //     Console.WriteLine($"{arr[i]} ");
+//     i++;
 // }
-
-// i = 0;
-// sum = 0;
-
-// while (i < n)                    //сумма элементов
-// {
-//     sum = sum + arr[i];
-// }
-
-// i = 0;
-// product = 1;
-
+//int i = 0;
+//int sum = 0;
+while (i < n)                    //сумма элементов
+{
+    sum = sum + arr[i];
+    i++;
+}
+//int i = 0;
+//int product = 1;
 // while (i < n)                    //произведение
 // {
 //     product = product * arr[i];
+//     i++;
 // }
+Console.WriteLine($"Сумма = {sum}");
+// Console.WriteLine($"Произведение = {product}");
 
-// Console.WriteLine(sum);
-// Console.WriteLine(product);
-
-int n = 10;
-int[] arr = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-int i = 0;
+// int n = 10;
+// int[] arr = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// int i = 0;
 
 // int FillArray(int[] arr)                    //заполняющий массив
 // {                                        
@@ -49,25 +48,25 @@ int i = 0;
 //     return arr[i];
 // }
 
-void PrintArray(int[] arr)                   //вывод массива на экран
-{
-    while (i < n)
-    {
-        Console.Write($"{arr[i]} ");
-        i = i + 1;
-    }
-}
+// void PrintArray(int[] arr)                   //вывод массива на экран
+// {
+//     while (i < n)
+//     {
+//         Console.Write($"{arr[i]} ");
+//         i = i + 1;
+//     }
+// }
 
-int sum = GetSumOfElements(int[] arr)     //сумма
-{
-    sum = 0;
-    while (i < n)
-    {
-        sum = sum + arr[i];
-        //i++;
-    }
-    return sum;
-}
+// int sum = GetSumOfElements(int[] arr)     //сумма
+// {
+//     sum = 0;
+//     while (i < n)
+//     {
+//         sum = sum + arr[i];
+//         //i++;
+//     }
+//     return sum;
+// }
 
 // int product = GetProductOfElements()
 // {
@@ -78,8 +77,8 @@ int sum = GetSumOfElements(int[] arr)     //сумма
 //     }
 // }
 //FillArray(arr);
-PrintArray(arr);
-GetSumOfElements(arr);
+// PrintArray(arr);
+// GetSumOfElements(arr);
 // GetProductOfElements(arr);
-Console.WriteLine(sum);
+// Console.WriteLine(sum);
 //Console.WriteLine(product);
