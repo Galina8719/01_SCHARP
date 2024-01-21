f3b34fc7deee88c9a44950159d6275a89626f45c
galna <galina_logistika@mail.ru>
2024-01-21 17:12:07 +0300

    commit

---
+++ Lesson3/Task7/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.WriteLine("Введите целое число:");
+// string input = Console.ReadLine();
+// int value = Convert.ToInt32(input);
+// Console.WriteLine ($"Введенное число = {value}");
+
+int size = 10;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    Console.WriteLine ($"Введите элемент массива (целое число):)");
+    string input = Console.ReadLine();
+    arr_int [i] = Convert.ToInt32(input);
+    i++;
+}
