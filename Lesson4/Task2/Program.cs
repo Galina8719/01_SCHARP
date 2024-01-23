0ab0d9925846f854ab225efeaff91bfe9298696a
galna <galina_logistika@mail.ru>
2024-01-23 15:23:34 +0300

    commit

--- Lesson4/Task2/Program.cs
+++ Lesson4/Task2/Program.cs
@@ -60,19 +60,4 @@ int GetSumOFDigits(int value)
     }
     return sum;
 }
-// void ShowMatrix(int[,] matrix)
-// {
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             Console.Write($"{matrix[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
 
-// int[,] matrix = CreateMatrix(4, 5);
-// ShowMatrix(matrix);
-
-// Console.WriteLine("Hello, World!");
--- Lesson4/Task3/Program.cs
+++ Lesson4/Task3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string s1 = "hello";
+
+// char[] ch_array = { 'w', 'o', 'r', 'l', 'd' };
+// string s2 = new String(ch_array);
+
+// string s3 = new String('a', 6); // результат "aaaaaa"
+
+//Задача 3. Считать с консоли строку, 
+// состоящую из цифр и латинских букв.
+// Сформировать новую строку, состоящую 
+// из букв исходной строки.
+
+string GetLetters(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+Console.WriteLine("Введите строку:");
+string str = Console.ReadLine();
+string result = GetLetters(str);
+Console.WriteLine(result);
