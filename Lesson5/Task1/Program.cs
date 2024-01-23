06c4b09d454a31534181a2db71b093bcdac31ec8
galna <galina_logistika@mail.ru>
2024-01-23 15:23:56 +0300

    commit

--- Lesson5/Task1/Program.cs
+++ Lesson5/Task1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// string[,,] data = new string[3,3,3];
+// data.SetValue("foo", 0, 1, 2 );
+// for (int i = data.GetLowerBound(0); i <= data.GetUpperBound(0); i++)
+//     for (int j = data.GetLowerBound(1); j <= data.GetUpperBound(1); j++)
+//         for (int k = data.GetLowerBound(2); k <= data.GetUpperBound(2); k++)
+//             Console.WriteLine("{0},{1},{2}: {3}", i, j, k, data[i,j,k]);
